381bed3723d04a7f3c6a03611df63f5412be7b64
lndsilva <lndsilva@hotmail.com>
2025-08-19 22:15:19 -0300

    trabalhando com estrutura de repetição

--- operadores.php
+++ operadores.php
@@ -6,6 +6,19 @@
 	echo("Operadores de multiplicação <br/>");
 	echo("Operadores de divisão <br/>");
 
-	echo("Insira a operação desejada: <br />");
+	echo("Utilizando operadores: <br />");
+	$num1 = 10;
+	$num2 = 2;
 
- ?>
+	$resp = $num1 + $num2;
+	$resp1 = $num1 - $num2;
+	$resp2 = $num1 * $num2;
+	$resp3 = $num1 / $num2;
+	$resp4 = $num1 % $num2;
+
+	echo ("A soma de $num1 e $num2 é: $resp <br />");
+	echo ("A subtração de $num1 e $num2 é: $resp1 <br />");
+	echo ("A multiplicação de $num1 e $num2 é: $resp2 <br />");
+	echo ("A divisão de $num1 e $num2 é: $resp3 <br />");
+	echo ("A resto divisão de $num1 e $num2 é: $resp4 <br />");
+?>
