f67cd7de95d877d5ed31900a5c9007acf21a8b09
lndsilva <lndsilva@hotmail.com>
2025-08-26 19:51:30 -0300

    criando funções em php

---
+++ funcoes.php
@@ -0,0 +1,18 @@
+<?php 
+
+	//criando uma função
+	function titulo(){
+		echo("Calculando valores: <br />");
+	}
+
+	function somaValor($num1,$num2){
+		$resp = $num1+$num2;
+		echo("A soma de $num1 e $num2 é $resp");
+	}
+
+	$valor1=10; $valor2=20;
+	//executar ou chamar uma função
+	titulo();
+	somaValor($valor1,$valor2);
+
+?>
