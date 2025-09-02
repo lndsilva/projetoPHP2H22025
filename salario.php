140b8f5ea374d4d081ebc83ee649909f8bf7f859
Laércio Silva <lndsilva@hotmail.com>
2025-09-02 19:39:02 -0300

    Add files via upload
    folha de pagamento

---
+++ salario.php
@@ -0,0 +1,18 @@
+<?php 
+
+	//Você deverá mostra no final
+	//Salário Folha: R$ 
+	//Desconto INSS: R$
+	//Desconto IR: R$
+	//Salário Líquido: R$
+
+	
+	echo("Sistema de folha de pagamento<br/><br/>");
+	echo "Salário folha: R$ <br/>";
+	echo "Desconto INSS: R$ <br/>";
+	echo "Desconto IR: R$ <br/>";
+	echo "Salário líquido: R$ <br/>";
+
+
+
+ ?>
